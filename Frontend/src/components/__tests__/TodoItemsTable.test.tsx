06d782e1441f5b6fa4f2cf09d04f4b36ba1fae8c
Adrian Rivers <adrianlrivers@gmail.com>
2022-03-15 00:38:20 +1300

    :white_check_mark: Add basic mock api call test for table component

--- Frontend/src/components/__tests__/TodoItemsTable.test.tsx
+++ Frontend/src/components/__tests__/TodoItemsTable.test.tsx
@@ -1,49 +1,57 @@
-import { render, fireEvent, waitFor } from '@testing-library/react'
+import { render, waitFor, act } from '@testing-library/react'
 import React from 'react'
 import TodoItemsTable from '../TodoItemsTable'
 import { TodoItem } from '../../types'
+import axios from 'axios'
 
-const TodoItemsMock: TodoItem[] = [
+const TODO_ITEMS_MOCK: TodoItem[] = [
   {
-    id: 1,
+    id: '1',
     description: 'Buy milk',
     isCompleted: false,
   },
   {
-    id: 2,
-    description: 'Buy cheese',
-    isCompleted: false,
+    id: '2',
+    description: 'Pay bills',
+    isCompleted: true,
   },
 ]
 
-const handleMarkAsCompletedMock = jest.fn()
-
 describe('TodoItemsTable', () => {
-  it('should NOT render a table of todo items if none are passed to the component', () => {
-    const { container } = render(<TodoItemsTable todoItems={[]} handleMarkAsCompleted={handleMarkAsCompletedMock} />)
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
 
+  it('should NOT render a table of todo items if none are passed to the component', () => {
+    const { container, getByText } = render(<TodoItemsTable />)
+
+    getByText('You currently have no todo items in your todo list 🙂')
     expect(container.querySelector('table')).toBeNull()
     expect(container).toMatchSnapshot()
   })
 
   it('should render a TodoItemsTable component', async () => {
-    const { container, getByText, queryByText, getAllByText } = render(
-      <TodoItemsTable todoItems={TodoItemsMock} handleMarkAsCompleted={handleMarkAsCompletedMock} />
-    )
-    const markAsCompletedButtons = getAllByText('Mark as completed') as HTMLButtonElement[]
-
-    getByText(TodoItemsMock[0].description)
-    expect(markAsCompletedButtons.length).toBe(2)
-
-    fireEvent.click(markAsCompletedButtons[0])
-
-    waitFor(() => {
-      expect(queryByText(TodoItemsMock[0].description)).toBe(null)
-      expect(getByText('Woohoo', { exact: false })).toBeInTheDocument()
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({
+      data: [...TODO_ITEMS_MOCK],
     })
 
-    expect(handleMarkAsCompletedMock).toHaveBeenCalledTimes(1)
-    expect(handleMarkAsCompletedMock).toHaveBeenCalledWith(TodoItemsMock[0].id, TodoItemsMock[0].description)
-    expect(container).toMatchSnapshot()
+    const { container, getByText, queryAllByText } = render(<TodoItemsTable />)
+
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve))
+    })
+
+    await waitFor(() => {
+      const markAsCompleteButtons = queryAllByText('Mark as completed')
+      const completedTodoItems = queryAllByText('Woohoo 🥳')
+
+      getByText(TODO_ITEMS_MOCK[0].description)
+      getByText(TODO_ITEMS_MOCK[1].description)
+
+      expect(markAsCompleteButtons.length).toBe(1)
+      expect(completedTodoItems.length).toBe(1)
+
+      expect(container).toMatchSnapshot()
+    })
   })
 })
